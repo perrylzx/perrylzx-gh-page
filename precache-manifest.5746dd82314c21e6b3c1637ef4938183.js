2ad49e045b48bae88d118e3ab51d190df8074fbc
perrylzx <46482518+perrylzx@users.noreply.github.com>
2020-04-21 15:16:19 +0800

    Updates

---
+++ precache-manifest.5746dd82314c21e6b3c1637ef4938183.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c533e729315aa47542c66bba2ad97e36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e562d6e1e88e9701ea97",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "9bea13b1665ed382fd08",
+    "url": "/static/css/main.19c8e602.chunk.css"
+  },
+  {
+    "revision": "e562d6e1e88e9701ea97",
+    "url": "/static/js/2.3d1fdabf.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.3d1fdabf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bea13b1665ed382fd08",
+    "url": "/static/js/main.1da773fa.chunk.js"
+  },
+  {
+    "revision": "c051594adee5e6dc7456",
+    "url": "/static/js/runtime-main.52ec50ca.js"
+  },
+  {
+    "revision": "9eb78756da72bb8b4e0fc37a1421a989",
+    "url": "/static/media/company_logo.9eb78756.png"
+  },
+  {
+    "revision": "64eae0425ca07bc2979e4a539e463b86",
+    "url": "/static/media/goggle.64eae042.png"
+  },
+  {
+    "revision": "dd1dc864c3ae1c491d4ff4e85c2c63ce",
+    "url": "/static/media/instagram.dd1dc864.png"
+  },
+  {
+    "revision": "3671b4265e916a6a916b0f90c3832a37",
+    "url": "/static/media/pitchbooker.3671b426.png"
+  }
+]);
